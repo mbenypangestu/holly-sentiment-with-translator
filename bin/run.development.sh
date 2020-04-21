656116de210ce2d584bfe449d1337a9aaa194ec6
mygetzu <mbenypangestu@gmail.com>
2020-04-21 11:26:30 +0700

    Update bin sh

--- bin/run.development.sh
+++ bin/run.development.sh
@@ -4,4 +4,4 @@ git commit -m "Update"
 git pull origin master
 ps aux | grep 'get_sentiment_degree.py' | grep -v grep | awk '{print $2}' | xargs kill
 ps aux | grep 'get_temporal_processing.py' | grep -v grep | awk '{print $2}' | xargs kill
-nohup python3 main.py &
+nohup python3 get_sentiment_degree.py &
