9cfe077124211ef93ab291300a8826531096d128
mygetzu <mbenypangestu@gmail.com>
2020-04-21 11:20:23 +0700

    Update sentiment processing db

--- database/mongo_service.py
+++ database/mongo_service.py
@@ -17,7 +17,7 @@ class MongoService:
     def connect_mongo(self):
         try:
             self.client = MongoClient(host=self.host, port=self.port)
-            self.db = self.client.holly_production
+            self.db = self.client.holly_dev
 
             print("[", self.now, "]---> Success connecting to database!")
         except:
