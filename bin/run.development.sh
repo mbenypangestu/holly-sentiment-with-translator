0787300a99d7468fd1d275103d0b8ddd3476edf2
mygetzu <mbenypangestu@gmail.com>
2020-06-24 02:13:57 +0700

    Update crontab

--- bin/run.development.sh
+++ bin/run.development.sh
@@ -4,4 +4,4 @@ git commit -m "Update"
 git pull origin master
 ps aux | grep 'start_translate_sentiment.py' | grep -v grep | awk '{print $2}' | xargs kill
 ps aux | grep 'start_temporal_processing.py' | grep -v grep | awk '{print $2}' | xargs kill
-nohup python3 start_translate_sentiment.py &
+nohup /usr/bin/env python3 /home/mygetzu/projects/holly-sentiment-with-translator/start_translate_sentiment.py > /tmp/listener.log 2>&1 &
